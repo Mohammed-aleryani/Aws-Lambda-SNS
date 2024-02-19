4a27962ea70cb61a30cf56abcf890ce3ccc62412
Eryani <mym.aleryani@gmail.com>
2024-02-19 21:24:32 +0100

    init commit

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import boto3
 
-#Configure the lambda to be triggered in S3 put and post events
-
-s3Client=boto3.client('s3')
-snsClient=boto3.client('sns')
+s3_client = boto3.client('s3')
+sns_client = boto3.client('sns')
+sns_topic_arn = '<Topic Arn>'  # Replace with your SNS topic ARN
 
 def lambda_handler(event, context):
-    
-    bucket= event['Records'][0]['s3']['bucket']['name']
-    key=event["Records"][0]['s3']['object']['key']  #File name
-    
-    
-    response=s3Client.get_object(Bucket=bucket,Key=key)
-    
-    file_content = response['Body'].read().decode("utf-8")
-    words_count=len(file_content.split())
-    sendEmail( key , words_count)
-    
+    try:
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = event['Records'][0]['s3']['object']['key']  # File name
+        
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        file_content = response['Body'].read().decode('utf-8')
+        words_count = len(file_content.split())
+        
+        send_email(key, words_count)
+    except Exception as e:
+        print(f'An error occurred: {e}')
+        raise e  # Rethrow the exception to trigger a Lambda retry
 
+def send_email(file_name, count):
+    try:
+        message = f'The word count in the {file_name} file is {count}.'
+        sns_client.publish(TopicArn=sns_topic_arn, Message=message)
+    except Exception as e:
+        print(f'Failed to send email: {e}')
 
-def sendEmail(file_name, count):
-    
-    message= 'The word count in the {} file is {} . '.format(file_name , count)
-
-    
-    snsClient.publish(
-    	#Replace <Topic Arn>
-        TopicArn='<Topic Arn>',
-        Message=message,
-        )
